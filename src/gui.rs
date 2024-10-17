2e4e41b62a786d8c6cbefd0776128b12922bf82e
nyx <nnyyxxxx@protonmail.com>
2024-10-16 21:56:59 -0400

    more

--- src/gui.rs
+++ src/gui.rs
@@ -2,31 +2,53 @@ use gtk::{
     gio, glib, prelude::*, Application, ApplicationWindow, Button, FlowBox, Image, ScrolledWindow,
 };
 use std::cell::RefCell;
-use std::collections::HashMap;
+use std::collections::VecDeque;
 use std::fs;
 use std::io::Read;
 use std::path::{Path, PathBuf};
 use std::rc::Rc;
 
 const CONFIG_FILE: &str = "~/.config/hyprwall/config.ini";
+const BATCH_SIZE: usize = 15;
 
-struct ImageCache {
-    cache: HashMap<String, gtk::Image>,
+struct ImageLoader {
+    queue: VecDeque<PathBuf>,
+    current_folder: Option<PathBuf>,
 }
 
-impl ImageCache {
+impl ImageLoader {
     fn new() -> Self {
         Self {
-            cache: HashMap::new(),
+            queue: VecDeque::new(),
+            current_folder: None,
         }
     }
 
-    fn get(&self, path: &str) -> Option<gtk::Image> {
-        self.cache.get(path).cloned()
+    fn load_folder(&mut self, folder: &Path) {
+        self.queue.clear();
+        self.current_folder = Some(folder.to_path_buf());
+        if let Ok(entries) = fs::read_dir(folder) {
+            for entry in entries.filter_map(Result::ok) {
+                if let Ok(file_type) = entry.file_type() {
+                    if file_type.is_file() {
+                        let path = entry.path();
+                        if let Some(extension) = path.extension() {
+                            if ["png", "jpg", "jpeg"].contains(&extension.to_str().unwrap_or("")) {
+                                self.queue.push_back(path);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
-    fn insert(&mut self, path: String, image: gtk::Image) {
-        self.cache.insert(path, image);
+    fn next_batch(&mut self) -> Vec<PathBuf> {
+        self.queue.drain(..BATCH_SIZE.min(self.queue.len())).collect()
+    }
+
+    fn has_more(&self) -> bool {
+        !self.queue.is_empty()
     }
 }
 
@@ -59,15 +81,15 @@ pub fn build_ui(app: &Application) {
     scrolled_window.set_child(Some(&flowbox));
 
     let flowbox_ref = Rc::new(RefCell::new(flowbox));
-    let cache = Rc::new(RefCell::new(ImageCache::new()));
+    let image_loader = Rc::new(RefCell::new(ImageLoader::new()));
 
     let choose_folder_button = Button::with_label("Change wallpaper folder");
     let flowbox_clone = Rc::clone(&flowbox_ref);
-    let cache_clone = Rc::clone(&cache);
+    let image_loader_clone = Rc::clone(&image_loader);
     let window_weak = window.downgrade();
     choose_folder_button.connect_clicked(move |_| {
         if let Some(window) = window_weak.upgrade() {
-            choose_folder(&window, &flowbox_clone, &cache_clone);
+            choose_folder(&window, &flowbox_clone, &image_loader_clone);
         }
     });
 
@@ -78,15 +100,23 @@ pub fn build_ui(app: &Application) {
     window.set_child(Some(&main_box));
 
     let flowbox_clone = Rc::clone(&flowbox_ref);
-    let cache_clone = Rc::clone(&cache);
+    let image_loader_clone = Rc::clone(&image_loader);
     window.connect_show(move |_| {
         if let Some(last_path) = load_last_path() {
             let flowbox_clone2 = Rc::clone(&flowbox_clone);
-            let cache_clone2 = Rc::clone(&cache_clone);
+            let image_loader_clone2 = Rc::clone(&image_loader_clone);
             glib::idle_add_local(move || {
-                load_images(&last_path, &flowbox_clone2, &cache_clone2);
+                load_images(&last_path, &flowbox_clone2, &image_loader_clone2);
                 glib::ControlFlow::Continue
             });
+        }
+    });
+
+    let flowbox_clone = Rc::clone(&flowbox_ref);
+    let image_loader_clone = Rc::clone(&image_loader);
+    scrolled_window.connect_edge_reached(move |_, pos| {
+        if pos == gtk::PositionType::Bottom {
+            load_more_images(&flowbox_clone, &image_loader_clone);
         }
     });
 
@@ -96,7 +126,7 @@ pub fn build_ui(app: &Application) {
 fn choose_folder(
     window: &ApplicationWindow,
     flowbox: &Rc<RefCell<FlowBox>>,
-    cache: &Rc<RefCell<ImageCache>>,
+    image_loader: &Rc<RefCell<ImageLoader>>,
 ) {
     let dialog = gtk::FileChooserDialog::new(
         Some("Change wallpaper folder"),
@@ -113,11 +143,11 @@ fn choose_folder(
     }
 
     let flowbox_clone = Rc::clone(flowbox);
-    let cache_clone = Rc::clone(cache);
+    let image_loader_clone = Rc::clone(image_loader);
     dialog.connect_response(move |dialog, response| {
         if response == gtk::ResponseType::Accept {
             if let Some(folder) = dialog.file().and_then(|f| f.path()) {
-                load_images(&folder, &flowbox_clone, &cache_clone);
+                load_images(&folder, &flowbox_clone, &image_loader_clone);
                 save_last_path(&folder);
             }
         }
@@ -127,45 +157,55 @@ fn choose_folder(
     dialog.show();
 }
 
-fn load_images(folder: &Path, flowbox: &Rc<RefCell<FlowBox>>, cache: &Rc<RefCell<ImageCache>>) {
-    let flowbox = flowbox.borrow_mut();
-    while let Some(child) = flowbox.first_child() {
-        flowbox.remove(&child);
+fn load_images(folder: &Path, flowbox: &Rc<RefCell<FlowBox>>, image_loader: &Rc<RefCell<ImageLoader>>) {
+    {
+        let mut flowbox = flowbox.borrow_mut();
+        while let Some(child) = flowbox.first_child() {
+            flowbox.remove(&child);
+        }
     }
 
-    let mut cache = cache.borrow_mut();
+    {
+        let mut image_loader = image_loader.borrow_mut();
+        image_loader.load_folder(folder);
+    }
 
-    if let Ok(entries) = fs::read_dir(folder) {
-        for entry in entries.filter_map(Result::ok) {
-            if let Ok(file_type) = entry.file_type() {
-                if file_type.is_file() {
-                    if let Some(path) = entry.path().to_str() {
-                        if path.ends_with(".png")
-                            || path.ends_with(".jpg")
-                            || path.ends_with(".jpeg")
-                        {
-                            let image = if let Some(cached_image) = cache.get(path) {
-                                cached_image
-                            } else {
-                                let new_image = Image::from_file(path);
-                                new_image.set_pixel_size(250);
-                                cache.insert(path.to_string(), new_image.clone());
-                                new_image
-                            };
+    load_more_images(flowbox, image_loader);
+}
 
-                            let button = Button::builder().child(&image).build();
+fn load_more_images(flowbox: &Rc<RefCell<FlowBox>>, image_loader: &Rc<RefCell<ImageLoader>>) {
+    let batch;
+    let has_more;
+    {
+        let mut image_loader = image_loader.borrow_mut();
+        batch = image_loader.next_batch();
+        has_more = image_loader.has_more();
+    }
 
-                            let path_clone = path.to_string();
-                            button.connect_clicked(move |_| {
-                                crate::set_wallpaper(&path_clone);
-                            });
+    {
+        let mut flowbox = flowbox.borrow_mut();
+        for path in batch {
+            let image = Image::from_file(&path);
+            image.set_pixel_size(250);
 
-                            flowbox.insert(&button, -1);
-                        }
-                    }
-                }
-            }
+            let button = Button::builder().child(&image).build();
+
+            let path_clone = path.to_str().unwrap_or("").to_string();
+            button.connect_clicked(move |_| {
+                crate::set_wallpaper(&path_clone);
+            });
+
+            flowbox.insert(&button, -1);
         }
+    }
+
+    if has_more {
+        let flowbox_clone = Rc::clone(flowbox);
+        let image_loader_clone = Rc::clone(image_loader);
+        glib::idle_add_local(move || {
+            load_more_images(&flowbox_clone, &image_loader_clone);
+            glib::ControlFlow::Continue
+        });
     }
 }
 
